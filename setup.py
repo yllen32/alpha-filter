0eeb93d6b04b672fd54de0efbeb7dd36ff3015a9
yllen32 <furtov1995@yandex.ru>
2023-04-30 02:07:14 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='alpha-filter',
+    version='0.4',
+    description='differential filter',
+    py_modules=['alpha-filter'],
+    python_requires='>=3.8'
+)
