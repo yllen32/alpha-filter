1c8c9f4713d430144b9e6e04ee9b81e49b42542f
yllen32 <furtov1995@yandex.ru>
2023-05-28 14:59:43 +0300

    vrsion updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='alpha-filter',
-    version='0.4',
+    version='0.9.1',
     description='differential filter',
     py_modules=['alpha-filter'],
     python_requires='>=3.8'
